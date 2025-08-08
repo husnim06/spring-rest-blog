b12b8439b82e2a520a5eea8b5a48b961af696f2c
husnim <kazancevvlad572@gmail.com>
2025-08-08 17:33:10 +0500

    добавлен котроллер "/"

---
+++ src/main/java/ru/husnim/todolist/controller/HomeController.java
@@ -0,0 +1,14 @@
+package ru.husnim.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui/index.html";
+    }
+    
+}
