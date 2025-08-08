906819df280185fc3e47c2c676827e13724ebff9
husnim <kazancevvlad572@gmail.com>
2025-08-08 21:37:50 +0500

    Изменено название проекта

---
+++ src/main/java/ru/husnim/spring_rest_blog/SpringRestBlogApplication.java
@@ -0,0 +1,13 @@
+package ru.husnim.spring_rest_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestBlogApplication.class, args);
+	}
+
+}
